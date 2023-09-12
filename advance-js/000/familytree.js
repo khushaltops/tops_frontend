c53ece74c96d65f1d1d3dcc4eec8bb400fe1f6a0
khushal kothiya <khushalkothiya5@gmail.com>
2023-09-12 15:38:49 +0530

    #commit

---
+++ advance-js/000/familytree.js
@@ -0,0 +1,59 @@
+let familytree = {
+    name:'test',
+    age:98,
+    child:[
+        {
+            id:10001,
+            name:"vishnubhai",
+            age:55,
+            occupation:'retired',
+            vehicle:[
+               {
+                name:'passion',
+                Number:5040,
+               },
+               {
+                name:'thar',
+                number:1111,
+               },
+            ],
+            son: [
+                {
+                    name:'urvish',
+                    age:22,
+                },
+                {
+                    name:'toral',
+                    age:30,
+                },
+            ],
+        },
+        {
+            id:10002,
+            name:'jitubhai',
+            age:42,
+            occupation:'retired',
+            vehicle: [
+                {
+                    name:'passion',
+                    number:5040,
+                },
+                {
+                    name:'thar',
+                    number:1111,
+                },
+            ],
+            son:[
+                {
+                    name:'jigar',
+                    age:29,
+                },
+                {
+                    name:'kavya',
+                    age:32,
+                },
+            ],
+        },
+    ],
+
+}
