1f748459d4ab9685e35841cba718f95087d44800
khushal kothiya <khushalkothiya5@gmail.com>
2023-09-29 10:20:36 +0530

    #commit

--- advance-js/2/function.js
+++ advance-js/2/function.js
@@ -1,17 +1,19 @@
 function fun(a, b, c) {
-  console.log("--->");
-  return [1, 2, 3];
+  console.log("----->")
+  return [1, 2, 2]
 }
 
-let x = fun(1, [1, 2, 3, 3]);
+
+let x = fun(1, [1, 2, 3, 3])
 
 let fun2 = function () {
-  console.log("------------>");
-};
 
-fun2();
+  console.log("----------------->")
+
+}
+fun2()
 
 const fun3 = (x) => {
-  console.log("---fun3---->", x);
-};
-fun3(100000);
+  console.log("---fun3------------>", x)
+}
+fun3(100000)
